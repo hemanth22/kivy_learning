fd29c2b42a6affb6683d1ceac3d3fa66ac08eaa0
hemanth22 <hemanth22hemu@gmail.com>
2021-06-17 13:03:27 +0530

    adding height and width under GridLayout

--- Tutorial4/input3.py
+++ Tutorial4/input3.py
@@ -6,61 +6,66 @@ from kivy.uix.textinput import TextInput
 from kivy.uix.button import Button
 
 class MyGridLayout(GridLayout):
-	# initialize inifinte keyword
-	def __init__(self, **kwargs):
-		# call grid layout constructor
-		super(MyGridLayout, self).__init__(**kwargs)
+    # initialize inifinte keyword
+    def __init__(self, **kwargs):
+        # call grid layout constructor
+        super(MyGridLayout, self).__init__(**kwargs)
 
-		# Set columns 
-		self.cols = 1
-		# Create a second layout
-		self.top_grid = GridLayout(row_force_default=True,row_default_height=40,col_force_default=True,col_default_width=100)
-		self.top_grid.cols = 2
+        # Set columns 
+        self.cols = 1
+        self.row_force_default = True
+        self.row_default_height=120
+        self.col_force_default=True
+        self.col_default_width=100
 
-
-		# Add widgets
-		self.top_grid.add_widget(Label(text="Name: "))
-		# Add Input Box
-		self.name = TextInput(multiline=False)
-		self.top_grid.add_widget(self.name)
+        # Create a second layout
+        self.top_grid = GridLayout(row_force_default=True,row_default_height=40,col_force_default=True,col_default_width=100)
+        self.top_grid.cols = 2
 
 
-		# Add widgets
-		self.top_grid.add_widget(Label(text="Favorite Pizza: "))
-		# Add Input Box
-		self.pizza = TextInput(multiline=False)
-		self.top_grid.add_widget(self.pizza)
+        # Add widgets
+        self.top_grid.add_widget(Label(text="Name: "))
+        # Add Input Box
+        self.name = TextInput(multiline=False)
+        self.top_grid.add_widget(self.name)
 
 
-		# Add widgets
-		self.top_grid.add_widget(Label(text="Favorite color: "))
-		# Add Input Box
-		self.color = TextInput(multiline=False)
-		self.top_grid.add_widget(self.color)
+        # Add widgets
+        self.top_grid.add_widget(Label(text="Favorite Pizza: "))
+        # Add Input Box
+        self.pizza = TextInput(multiline=False)
+        self.top_grid.add_widget(self.pizza)
 
-		# Add the new top_grid to our app
-		self.add_widget(self.top_grid)
 
-		#Create a Submit Button
-		self.submit = Button(text="Submit", font_size=32, size_hint_y=None, height=50, size_hint_x=None, width=200)
-		# Bind the button
-		self.submit.bind(on_press=self.press)
-		self.add_widget(self.submit)
+        # Add widgets
+        self.top_grid.add_widget(Label(text="Favorite color: "))
+        # Add Input Box
+        self.color = TextInput(multiline=False)
+        self.top_grid.add_widget(self.color)
 
-	def press(self, instance):
-		name = self.name.text
-		pizza = self.pizza.text
-		color = self.color.text
-		#print(f'Hello {name}, your pizza {pizza} is getting ready in {color} colour')
-		self.add_widget(Label(text=f'Hello {name}, your {pizza} pizza is getting ready in {color} colour'))
-		# Clear fields
-		self.name.text = ""
-		self.pizza.text = ""
-		self.color.text = ""
+        # Add the new top_grid to our app
+        self.add_widget(self.top_grid)
+
+        #Create a Submit Button
+        self.submit = Button(text="Submit", font_size=32, size_hint_y=None, height=50, size_hint_x=None, width=200)
+        # Bind the button
+        self.submit.bind(on_press=self.press)
+        self.add_widget(self.submit)
+
+    def press(self, instance):
+        name = self.name.text
+        pizza = self.pizza.text
+        color = self.color.text
+        #print(f'Hello {name}, your pizza {pizza} is getting ready in {color} colour')
+        self.add_widget(Label(text=f'Hello {name}, your {pizza} pizza is getting ready in {color} colour'))
+        # Clear fields
+        self.name.text = ""
+        self.pizza.text = ""
+        self.color.text = ""
 
 class MyApp(App):
-	def build(self):
-		return MyGridLayout()
+    def build(self):
+        return MyGridLayout()
 
 if __name__ == '__main__':
-	MyApp().run()
+    MyApp().run()
